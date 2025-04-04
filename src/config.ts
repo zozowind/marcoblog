04aa009c5c227b68db23ffc867661464b8b6a0e6
Marco Chen <zozowind@gmail.com>
2025-04-04 11:37:24 +0800

    feat(config): update site and profile information
    - Changed site title to 'Marco's Blog' and subtitle to '立而不惑，知而能行'.
    - Updated language setting to 'zh_CN'.
    - Modified GitHub link in the navigation bar to point to 'https://github.com/zozowind/marcoblog'.
    - Updated profile name to 'Marco Chen' and bio to 'Full Stack AI Developer'.
    - Changed Twitter link to 'https://twitter.com/zozowind'.

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  title: 'Marco\'s Blog',
+  subtitle: '立而不惑，知而能行',
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
@@ -44,7 +44,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/zozowind/marcoblog',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
@@ -52,15 +52,15 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'Marco Chen',
+  bio: 'Full Stack AI Developer',
   links: [
     {
       name: 'Twitter',
       icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://twitter.com/zozowind',
     },
     {
       name: 'Steam',
@@ -70,7 +70,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/zozowind/marcoblog',
     },
   ],
 }
